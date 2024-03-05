104de15ca2c70769bbaf7cfdc83a9af205b93c95
Arun raj <113260000+Arun-cloud-dev@users.noreply.github.com>
2024-03-05 07:32:18 +0530

    day 7

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -1,9 +1,19 @@
 import { useState } from 'react'
 
 import './App.css'
+import Card from './component/Card'
 
+let myObj = { 
+  name : "Arun",
+  age: 25,
+  address :{
+    city:'Channai',
+    state:'TN',
+    country:'India'
+  }
+}
 
-
+let newArr = [1, 2,3 ,4,5,6,7,8,9]
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +21,9 @@ function App() {
   return (
     <>
       <h1 className='text-3xl bg-green-500 p-3'>vite + tailwind</h1>
+      <Card username="Arun" myArr={newArr} />
+      <Card username='Joe' post= 'Staff Engneer'/>
+      <Card />
 
     </>
 
--- 03tailwindprops/src/component/Card.jsx
+++ 03tailwindprops/src/component/Card.jsx
@@ -1,14 +1,34 @@
-import React from 'react'
+import React from 'react';
 
-function Card() {
+function Card({username = "AR" , post = "Not assigned Yet"}) {
+  // console.log(props)
   return (
     <div>
-        <img src='https://www.livemint.com/lm-img/img/2024/02/28/600x338/TESLA-COMPENSATION-RULING-0_1707527438592_1709081669402.JPG' alt='elon musk'></img>
-        <h1 className='text-2xl bg-green-500 p-3 rounded '>a card for pgotos</h1>
-        <p>lorem1 askhga as a a adadasdswkjdghkjahs fas  jhgsdfjgsa a kjhgsajdfg  skgh  
-        </p>
+      <figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
+        <img
+          className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto"
+          src="https://arunraj-website-portfolio.s3.jp-tok.cloud-object-storage.appdomain.cloud/images/AR.jpeg"
+          alt=""
+          width="384"
+          height="512"
+        />
+        <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
+          <blockquote>
+            <p className="text-lg font-medium">
+              “Tailwind CSS is the only framework that I've seen scale on large teams. It’s easy to customize, adapts to any design, and the build size is tiny.”
+            </p>
+          </blockquote>
+          <figcaption className="font-medium">
+            <div className="text-sky-500 dark:text-sky-400">
+              {username}
+            </div>
+            <div className="text-slate-700 dark:text-slate-500">{post}</div>
+          </figcaption>
+        </div>
+      </figure>
     </div>
-  )
+  );
 }
 
-export default Card
+export default Card;
+
