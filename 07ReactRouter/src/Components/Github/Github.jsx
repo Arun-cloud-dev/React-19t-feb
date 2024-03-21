b7c9a20ec85f26ca0b804dedfa07f5a847df24c3
Arun raj <113260000+Arun-cloud-dev@users.noreply.github.com>
2024-03-21 20:44:03 +0530

    Update Github.jsx

--- 07ReactRouter/src/Components/Github/Github.jsx
+++ 07ReactRouter/src/Components/Github/Github.jsx
@@ -1,22 +1,27 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from 'react';
 
 function Github() {
+    const [data, setData] = React.useState(null);
 
-    const [data, setData]= React.useState(null)
     useEffect(() => {
-        fetch ('https://api.github.com/users/Arun-cloud-dev')
-        .then((response) => response.json())
-        .then(data => {
-            console.log(data)
-            setData(data)
-        })
-    } ,[])
+        fetch('https://api.github.com/users/Arun-cloud-dev')
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                setData(data);
+            });
+    }, []);
 
     return (
-        <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>Github followers: {data.followers}
-        <img src={data.avatar_url} width={300} alt="" />
+        <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>
+            {data && (
+                <>
+                    <p>Github followers: {data.followers}</p>
+                    <img src={data.avatar_url} width={300} alt='' />
+                </>
+            )}
         </div>
-      )
-    }
+    );
+}
 
-export default Github
+export default Github;
